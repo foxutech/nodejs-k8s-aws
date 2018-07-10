dabfef43c5ef37aa645cad73e168ad5b3cd1df9f
FoxuTech <prabulugar@gmail.com>
2018-07-10 21:02:57 +0530

    Update awsenv.sh

--- awsenv.sh
+++ awsenv.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 ## before start create a user, we need to expose the Access key and secret key of your account
-printf "Enter your AWS access key"
-read accesskey
+read -rep "Enter your AWS accesskey: \n" accessKey
 
-printf "Enter your AWS secret key"
-read secretKey
+read -rep "Enter your AWS accesskey: \n" secretKey
 
 export AWS_ACCESS_KEY_ID=$accessKey
 export AWS_SECRET_ACCESS_KEY=$secretKey
 
 # create IAM user, group and roles
-aws iam create-group --group-name kops
+read -rep "Enter your AWS user: \n" user
 
-aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess --group-name kops
-aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonRoute53FullAccess --group-name kops
-aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess --group-name kops
-aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/IAMFullAccess --group-name kops
-aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonVPCFullAccess --group-name kops
+aws iam create-group --group-name $user
 
-aws iam create-user --user-name kops
-aws iam add-user-to-group --user-name kops --group-name kops
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess --group-name $user
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonRoute53FullAccess --group-name $user
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess --group-name $user
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/IAMFullAccess --group-name $user
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonVPCFullAccess --group-name $user
+
+aws iam create-user --user-name $user
+aws iam add-user-to-group --user-name kops --group-name $user
